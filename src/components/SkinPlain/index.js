d8002b53b122f3afa0533b072e7677b8e59a99a4
Alejo Lang <alejolang123@gmail.com>
2023-02-25 19:32:05 -0300

    Add SkinPlain (Plain skin texture) and its funtions

---
+++ src/components/SkinPlain/index.js
@@ -0,0 +1,16 @@
+
+function SkinPlain ({ uuid }) {
+
+    return (
+        <div className="SkinSection PlainSection">
+            <strong className="SkinSectionTitle PlainSectionTitle">Skin Plain Texture</strong>
+            <img 
+                src={'https://crafatar.com/skins/' + uuid + '?default=MHF_Steve'}
+                alt='Skin Avatar'
+                className="SkinSectionImg AvatarSectionImg"
+            />
+        </div>
+    )
+}
+
+export default SkinPlain;
