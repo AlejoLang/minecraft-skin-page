2e49d86e59d2e309292b8cbbd7c0bb7a16b5a108
Alejo Lang <alejolang123@gmail.com>
2023-02-25 19:11:54 -0300

    Add component SkinAvatar and its functions

---
+++ src/components/SkinAvatar/index.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function SkinAvatar ({uuid}) {
+
+    const [showOverlay, setShowOverlay] = useState(false);
+
+    return (
+        <div className="SkinSection AvatarSection">
+            <strong className="SkinSectionTitle AvatarSectionTitle">Skin Avatar</strong>
+            <img 
+                src={
+                    showOverlay ? 'https://crafatar.com/avatars/' + uuid + '?default=MHF_Steve&overlay' :
+                                  'https://crafatar.com/avatars/' + uuid
+                }
+                alt='Skin Avatar'
+                className="SkinSectionImg AvatarSectionImg"
+            />
+            <button onClick={() => {setShowOverlay(!showOverlay)}} className="SkinSectionBtn AvatarSectionBtn">Toggle Overlay</button>
+        </div>
+    )
+}
+
+export default SkinAvatar;
