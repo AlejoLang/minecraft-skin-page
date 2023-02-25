4e5006cb867a133326ba7fd0393581e7986c130a
Alejo Lang <alejolang123@gmail.com>
2023-02-25 19:54:29 -0300

    Add strong tag to uuid displayed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 import './App.css';
+import SkinAvatar from './components/SkinAvatar';
+import SkinBody from './components/SkinBody';
+import SkinCape from './components/SkinCape';
+import SkinHead from './components/SkinHead';
+import SkinPlain from './components/SkinPlain';
 
 function App() {
   const [idToSearch, updateIdToSearch] = useState('');
@@ -8,18 +13,31 @@ function App() {
   const handleSearch = async (e) => {
     e.preventDefault();
     setUuid(idToSearch);
+    updateIdToSearch('');
   }
 
   return (
     <div className="App">
-      <h1>Minecraft Skin by UUID</h1>
-      <p>Search a minecraft user by his UUID (found it <a href='https://minecraftuuid.com/'>here</a>)</p>
+      <div className='appInfo'>
+        <h1>Minecraft Skin by UUID</h1>
+        <p>Search a minecraft user by his UUID (found it <a href='https://minecraftuuid.com/'>here</a>)</p>
+      </div>
       <form onSubmit={handleSearch}>
-        <input type="text" placeholder='Enter a UUID' value={idToSearch} onChange={(e) => {updateIdToSearch(e.target.value)}}/>
-        <button>0</button>
+        <input 
+          type="text" 
+          placeholder='Enter a UUID' 
+          value={idToSearch} 
+          onChange={(e) => {updateIdToSearch(e.target.value)}}
+          className='searchInput'
+        />
+        <button className='searchBtn'>0</button>
       </form>
-      {uuid ? <h2>Results for {uuid}</h2> : ''}
-      
+      {uuid ? <h2>Results for <strong>{uuid}</strong></h2> : ''}
+      <SkinAvatar uuid={uuid}/>
+      <SkinHead uuid={uuid}/>
+      <SkinBody uuid={uuid}/>
+      <SkinPlain uuid={uuid}/>
+      <SkinCape uuid={uuid}/>
     </div>
   );
 }
