2bc9ce61f544208854ab9c25de6064a4378182fe
Alejo Lang <alejolang123@gmail.com>
2023-02-26 23:28:35 -0300

    Add download button for the plain skin and cape textures

--- src/components/SkinCape/index.js
+++ src/components/SkinCape/index.js
@@ -1,6 +1,15 @@
 
 function SkinCape ({ uuid }) {
 
+    const downloadSkin = () => {
+        var link = document.createElement("a");
+        link.download = uuid + '_skin';
+        link.href = 'https://crafatar.com/capes/' + uuid + '.png';
+        link.setAttribute("target", "_blank");
+        link.click();
+        link.remove();
+    }
+
     return (
         <div className="SkinSection CapeSection">
             <strong className="SkinSectionTitle CapeSectionTitle">Skin Cape</strong>
@@ -9,6 +18,7 @@ function SkinCape ({ uuid }) {
                 alt='Skin Cape'
                 className="SkinSectionImg CapeSectionImg"
             />
+            <button className="SkinSectionBtn downloadSkinBtn" onClick={downloadSkin}>Download Cape</button>
         </div>
     )
 }
--- src/components/SkinPlain/index.js
+++ src/components/SkinPlain/index.js
@@ -1,6 +1,15 @@
 
 function SkinPlain ({ uuid }) {
 
+    const downloadSkin = () => {
+        var link = document.createElement("a");
+        link.download = uuid + '_skin';
+        link.href = 'https://crafatar.com/skins/' + uuid + '.png';
+        link.setAttribute("target", "_blank");
+        link.click();
+        link.remove();
+    }
+
     return (
         <div className="SkinSection PlainSection">
             <strong className="SkinSectionTitle PlainSectionTitle">Skin Plain Texture</strong>
@@ -9,6 +18,7 @@ function SkinPlain ({ uuid }) {
                 alt='Skin Avatar'
                 className="SkinSectionImg PlainSectionImg"
             />
+            <button className="SkinSectionBtn downloadSkinBtn" onClick={downloadSkin}>Download Skin</button>
         </div>
     )
 }
